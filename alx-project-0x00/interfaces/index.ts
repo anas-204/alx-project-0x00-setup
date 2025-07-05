b3c6235e37c94245f6c8b62eb3e2bccaa752d3ad
Anas <anaselsheikh2004@gmail.com>
2025-07-05 23:06:24 +0300

    created the button component

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -3,7 +3,7 @@ export interface PillProps {
 }
 export interface ButtonProps {
   title: string
-  styles : string
+  styles: string
   size?: 'small' | 'medium' | 'large'
   shape?: 'rounded-sm' | 'rounded-md' | 'rounded-full'
   onClick?: () => void
