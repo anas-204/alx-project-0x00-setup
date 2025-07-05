4fc90649a250e75cead324b155e168d01c61db90
Anas <anaselsheikh2004@gmail.com>
2025-07-05 22:43:53 +0300

    used the card component

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -4,11 +4,7 @@ const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Card />
     </div>
   )
 }
