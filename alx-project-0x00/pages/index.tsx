4d5508a0548bf7ab12df6179409c5cb0ffe5471f
Anas <anaselsheikh2004@gmail.com>
2025-07-05 22:42:45 +0300

    used the card component

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -4,7 +4,6 @@ const Home: React.FC = () => {
   return (
     <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
       <h1>Airbnb Application Clone system</h1>
-      <Card/>
       <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
     </main>
   )
